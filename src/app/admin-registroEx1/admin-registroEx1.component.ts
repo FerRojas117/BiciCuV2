fe38ed33f8d295904bb4474e0a8254d35f6bcf7f
Valeria <valeria@hotmail.com>
2019-02-19 18:13:25 -0600

    Componentes Primera versión con rutas

---
+++ src/app/admin-registroEx1/admin-registroEx1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { AdminMenu } from '../admin-menu/admin-menu.component';
+
+@Component({
+  templateUrl: './admin-registroEx1.component.html',
+  styleUrls: ['./admin-registroEx1.component.scss'],
+})
+export class AdminRegistroEx1 {
+
+}
