9eb0f0ee3c2973c28040a6e86a7089646c16ad09
Valeria <valeria@hotmail.com>
2019-02-23 21:54:53 -0600

    rutas correctas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { RecuperarContr } from './recuperar-contr/recuperar-contr.component';
 import { RecuperarContr2 } from './recuperar-contr2/recuperar-contr2.component';
 import { InicioR } from './inicio-reg/inicio-reg.component';
 import { Intro } from './intro/intro.component';
+import { AdminRegistroEx2 } from './admin-registroEx2/admin-registroEx2.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
@@ -19,6 +20,7 @@ const routes: Routes = [
   {path: 'aControlB', component: AdminControl},
   {path: 'aAccesos', component: AdminAccesos},
   {path: 'aRegE1', component: AdminRegistroEx1},
+  {path: 'aRegE2', component: AdminRegistroEx2},
   {path: 'aRegV', component: RegistroVig},
   {path: 'aExp', component: AdminExport},
   {path: 'aInicio', component: Admininicio},
